abf81cdb1e8c450aaa0bf68a6038268af20a8af8
Jookia <contact@jookia.org>
2022-09-25 06:32:00 +1000

    Initial commit

---
+++ other.py
@@ -0,0 +1,16 @@
+import test
+
+class MyCounter():
+	myval = 5
+	def increment(self):
+		self.myval += 5
+
+def add_two_strings(str1, str2):
+	str3 = str1 + str2
+	return str3
+
+for i in range(0, 15):
+	print(i)
+
+while True:
+	print(i)
